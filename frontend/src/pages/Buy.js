cc9eea5af70f9a0caed2f3607487ddd9c68e896b
ChrisLiutx <63918785+ChrisLiutx@users.noreply.github.com>
2021-10-23 23:28:10 -0700

    Update Buy.js

--- frontend/src/pages/Buy.js
+++ frontend/src/pages/Buy.js
@@ -1,23 +1,29 @@
-import { React } from 'react'
-import { Grid } from '@material-ui/core'
-import { ArtistList } from './ArtistList'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { web3Store } from '../stores/web3Store'
 import { ActiveDeals } from './ActiveDeals'
+import { ArtistList } from './ArtistList'
+import { EditProfile } from './EditProfile'
+import { useWeb3React } from "@web3-react/core"
+const GridWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+  `
 export const Buy = (props) => {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+
   return (
     <div
-      style={{ display: 'flex', height: 1000, backgroundColor: 'red', flexDirection: 'row' }}
-    >
-      <div style={{ height: '100%', backgroundColor: 'white', flex:1  }}>
-          <ArtistList></ArtistList>
-      </div>
-      <div style={{ height: '100%', backgroundColor: 'green', flex:1 }}>
-          <ActiveDeals isBuyer={true}></ActiveDeals>
-      </div>
-    </div>
-  )
-  return (
-    <div
-      style={{ display: 'flex', height: 1000, backgroundColor: 'red', flexDirection: 'row' }}
+      style={{
+        display: 'flex',
+        height: 1000,
+        flexDirection: 'row',
+      }}
     >
       {active ?
         <>
@@ -27,7 +33,7 @@ export const Buy = (props) => {
           <div style={{ height: '100%', backgroundColor: 'green', flex:1 }}>
               <ActiveDeals></ActiveDeals>
           </div>
-        </>
+       </>
         :
         <div style={{width:"100%"}}>
           <br></br>
@@ -40,3 +46,6 @@ export const Buy = (props) => {
 
   )
 }
+
+
+
